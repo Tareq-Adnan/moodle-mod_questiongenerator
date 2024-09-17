b36101130c9271104e48af966b11041363b34b7b
Tareq-Adnan <adnantareq2@gmail.com>
2024-09-17 15:21:22 +0600

    Plugin Skeleton

---
+++ lang/en/questiongenerator.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_questiongenerator
+ * @category    string
+ * @copyright   2024 Tarekul Islam <tarekul.islam@brainstation-23.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Question Generator';
+$string['modulenameplural'] = 'Questions Generator';
+$string['pluginname'] = 'Questions Generator';
+$string['questiongeneratorname'] = 'Name';
+$string['pluginadministration'] = 'Questions Generator Administration';
+$string['questiongenerator:addinstance'] = 'Add a new Questions Generator';
+$string['questiongenerator:myaddinstance'] = 'Add a new Questions Generator to the My Moodle page';
+$string['questiongenerator:view'] = "View Question Generator Activity";
